d875821a2d728d2508b5ed863498fe26f28b0fbd
Camilla Cortney <camilla.cortney@gmail.com>
2022-08-16 15:04:15 -0700

    404 company not found route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import { useLocalStorage } from "./hooks";
 import AppRoutes from "./AppRoutes";
+import JoblyApi from "./api";
+import { decodeToken } from 'react-jwt';
+import UserContext from "./UserContext";
 import './App.css';
+import Navbar from "./Navbar";
+
 
 function App() {
 
-  // const [token, setToken] = useLocalStorage("token", null);
+  const [token, setToken] = useLocalStorage("token", null);
+  const [user, setUser] = useState(null);
 
-  // const [user, setUser] = useState(null);
+  useEffect(() => {
+    async function getUserFromToken() {
+      JoblyApi.token = token;
+      let payload = decodeToken(token)
+      const user = await JoblyApi.getUser(payload.username)
+      setUser(user)
+    }
+    if (token) {
+      getUserFromToken()
+    }
+  }, [token])
 
+  const login = async (data) => {
+    let res = await JoblyApi.login(data);
+    setToken(res)
+  };
+
+  const signup = async (data) => {
+    let res = await JoblyApi.signup(data);
+    setToken(res)
+  }
+
+  const editProfile = async (data) => {
+    let res = await JoblyApi.editProfile(user.username, data);
+    setUser(res);
+  }
+
+  const applyForJob = async (jobId) => {
+    let res = await JoblyApi.applyForJob(user.username, jobId);
+    setUser({...user, applications: [...user.applications, res]})
+  }
+
+  const logout = () => {
+    JoblyApi.token = null;
+    setToken(null)
+    setUser(null)
+  }
 
   return (
-    <div className="App">
-      <AppRoutes/>
-    </div>
+    <UserContext.Provider value={user}>
+      <div className="App">
+        <Navbar logout={logout} />
+        <AppRoutes login={login} signup={signup} token={token} editProfile={editProfile} applyForJob={applyForJob}/>
+      </div>
+    </UserContext.Provider>
+
   );
 }
 

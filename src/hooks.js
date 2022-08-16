e28486a5dce00e612f338e2798e2464f445e43f2
Camilla Cortney <camilla.cortney@gmail.com>
2022-08-16 14:58:50 -0700

    another commit

--- src/hooks.js
+++ src/hooks.js
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react"
 
 const useLocalStorage = (key, defaultValue) => {
     const [state, setState] = useState(() => {
-        let value = JSON.parse(window.localStorage.getItem(key) || defaultValue);
+        let value = JSON.parse(localStorage.getItem(key) || defaultValue);
         return value;
     });
+
     useEffect(() => {
-        window.localStorage.setItem(key, state)
+        localStorage.setItem(key, JSON.stringify(state))
     }, [key, state])
 
     return [state, setState]
